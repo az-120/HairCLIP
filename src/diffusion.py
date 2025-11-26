02a95b8acdc8300a43c1ecc300ddc27711e90bdf
Arthur Zhao <adz6@duke.edu>
2025-11-26 15:06:01 -0500

    retry this for hair masking

--- src/diffusion.py
+++ src/diffusion.py
@@ -6,11 +6,11 @@ from diffusers import AutoPipelineForInpainting
 import matplotlib.pyplot as plt
 from hair_segmentation import get_editable_mask, get_hair_mask
 
-pipe = AutoPipelineForInpainting.from_pretrained(
-    "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
-    torch_dtype=torch.float16,
-    variant="fp16"
-).to("cuda")
+# pipe = AutoPipelineForInpainting.from_pretrained(
+#     "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
+#     torch_dtype=torch.float16,
+#     variant="fp16"
+# ).to("cuda")
 
 # pipe.enable_xformers_memory_efficient_attention() TODO: properly use this
 
@@ -27,39 +27,36 @@ mask_np_full = get_hair_mask(bgr)
 
 # Ensure mask is single-channel
 if len(mask_np_full.shape) == 3:
+    print('length thing')
     mask_np_full = mask_np_full[:, :, 0]
 
-# Convert to uint8 0–255 if needed
-if mask_np_full.max() <= 1:
-    mask_np_full = (mask_np_full * 255).astype(np.uint8)
-else:
-    mask_np_full = mask_np_full.astype(np.uint8)
-
+# Convert to uint8 0–255 (should always .max() <= 1 based on func)
+mask_np_full = (mask_np_full * 255).astype(np.uint8)
 
 target_size = (1024, 1024)
 
 rgb_small = cv2.resize(rgb, target_size, interpolation=cv2.INTER_AREA)
 mask_small = cv2.resize(mask_np_full, target_size, interpolation=cv2.INTER_NEAREST)
 
-cv2.imwrite("data/test/debug_mask_small.png", mask_small * 255)
+cv2.imwrite("data/test/debug_mask_small.png", mask_small)
 
-# Convert to PIL for diffusers
-image_pil = Image.fromarray(rgb_small)
-mask_pil = Image.fromarray(mask_small).convert("L")
+# # Convert to PIL for diffusers
+# image_pil = Image.fromarray(rgb_small)
+# mask_pil = Image.fromarray(mask_small).convert("L")
 
-prompt = "asian man with buzz cut"
+# prompt = "asian man with buzz cut"
 
-with torch.no_grad():
-    out = pipe(
-        prompt=prompt,
-        image=image_pil,
-        mask_image=mask_pil,
-        guidance_scale=7.5,
-        num_inference_steps=20,
-        strength=0.9
-    )
+# with torch.no_grad():
+#     out = pipe(
+#         prompt=prompt,
+#         image=image_pil,
+#         mask_image=mask_pil,
+#         guidance_scale=7.5,
+#         num_inference_steps=20,
+#         strength=0.9
+#     )
 
-result_pil = out.images[0]
+# result_pil = out.images[0]
 
-result_np = np.array(result_pil)[:, :, ::-1]
-cv2.imwrite("data/test/diffusion.png", result_np)
+# result_np = np.array(result_pil)[:, :, ::-1]
+# cv2.imwrite("data/test/diffusion.png", result_np)
