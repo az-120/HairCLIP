146a8f97b9430c7c2d0bb5d2a8d5c65252159b0d
Arthur Zhao <adz6@duke.edu>
2025-12-02 20:18:08 -0500

    testing paolo banchero

--- src/diffusion.py
+++ src/diffusion.py
@@ -13,7 +13,7 @@ pipe = AutoPipelineForInpainting.from_pretrained(
 
 # pipe.enable_xformers_memory_efficient_attention() TODO: properly use this
 
-bgr = cv2.imread("data/test/IMG_2817.png")
+bgr = cv2.imread("data/test/paolo.png")
 
 if bgr is None:
     raise ValueError("Image not found at data/test/IMG_2817.png")
